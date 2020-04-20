4b18c3579c22df07979ecc0e2881d7221a358fcd
GinoAntonel <ginoantonel.ga@gmail.com>
2020-04-20 18:37:45 -0300

    Validation form (estetico y logico)

--- Libreria/js/index.js
+++ Libreria/js/index.js
@@ -5,12 +5,15 @@ function calcularInversion(){
     var banco = document.getElementById("selectBanco");
     var bancoValue = banco.options[banco.selectedIndex].value;
     if (bancoValue == 1){var porcentajeBanco = 20.4;} //Mercado Pago
-    if (bancoValue == 2){var porcentajeBanco = 2.4;} // Banco Galicia
-    if (bancoValue == 3){var porcentajeBanco = 2.4;} // Banco Marco
-    if (bancoValue == 4){var porcentajeBanco = 2.4;} // Banco Nacion
+    if (bancoValue == 2){var porcentajeBanco = 23.5;} // Banco Galicia
+    if (bancoValue == 3){var porcentajeBanco = 27.08;} // Banco Marco
+    if (bancoValue == 4){var porcentajeBanco = 21;} // Banco Nacion
     var porcentaje = (tiempo * porcentajeBanco)/365;
     var dineroEstimado = (dinero * porcentaje)/100;
-    document.getElementById("labelDineroTotal").innerHTML = dineroEstimado.toFixed(2);
+    console.log(dinero)
+    if (dinero > 0 && dinero <= 500000 && tiempo > 0 && tiempo <= 365 ){
+        document.getElementById("labelDineroTotal").innerHTML = dineroEstimado.toFixed(2);
+    }
 }
 
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -23,8 +26,8 @@ var chart = new Chart(ctx, {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(74,128,140)',
+            borderColor: 'rgba(3,3,8,0.75)',
             data: [0, 10, 20, 2, 20, 30, 45]
         }]
     },
@@ -36,9 +39,9 @@ var chart = new Chart(ctx, {
 (function() {
     'use strict';
     window.addEventListener('load', function() {
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
-// Loop over them and prevent submission
+        // Loop over them and prevent submission
         var validation = Array.prototype.filter.call(forms, function(form) {
             form.addEventListener('submit', function(event) {
                 if (form.checkValidity() === false) {
